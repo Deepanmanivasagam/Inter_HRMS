89e9720834c58e92803c920a0810a43d92226c08
deepan6381540084 <deepanmanivasagam@gmail.com>
2025-04-02 13:55:29 +0530

    files

--- model/project-model.js
+++ model/project-model.js
@@ -3,45 +3,44 @@ const Schema = mongoose.Schema;
 const moment = require('moment');
 
 const projectSchema = new Schema({
-    projectName: {
-        type: String,
-        required: true,
+    projectName:{
+        type:String,
+        required:true,
     },
-    teamMembers: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Register'
+    teamMembers:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Register'
     }],
-    startDate: {
-        type: Date,
-        required: true,
+    startDate:{
+        type:Date,
+        required:true,
     },
-    endDate: {
-        type: Date,
-        required: true,
+    endDate:{
+        type:Date,
+        required:true,
     },
-    dueDays: {
-        type: Number,
-        default: 0
+    dueDays:{
+        type:Number,
+        default:0
     },
-    overdueDays: {
-        type: Number,
-        default: 0
+    overdueDays:{
+        type:Number,
+        default:0
     }
-}, {
-    timestamps: true,
-    versionKey: false
+},{
+    timestamps:true,
+    versionKey:false
 });
 
-projectSchema.pre('save', function (next) {
+projectSchema.pre('save', function(next){
 
     this.dueDays = moment(this.endDate).diff(moment(this.startDate), 'days');
     const today = moment().startOf('day');
-    if (today.isAfter(this.endDate)) {
-        this.overdueDays = today.diff(moment(this.endDate), 'days');
-    } else {
+    if(today.isAfter(this.endDate)){
+        this.overdueDays = today.diff(moment(this.endDate),'days');
+    }else{
         this.overdueDays = 0;
     }
-
     next();
 });
 
